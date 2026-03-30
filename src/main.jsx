3a8051c3d52ccb697068abb504034cd3cfec9905
YourName <YourEmail@example.com>
2026-03-30 16:35:08 +0300

    System_Rebuild: Clean_Slate_v1.0

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import SajjadDragon from './SajjadDragon.jsx'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <SajjadDragon />
+  </StrictMode>,
+)
